67d5d02586ab2f0e52d495983846065779641467
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 00:33:31 +0530

    Update __main__.py

--- bunny/__main__.py
+++ bunny/__main__.py
@@ -6,7 +6,7 @@ user.start()
 if LOG:
   try:
     user.send_message(S_T_F_U_09, "Userbot Started Successfully.... !")
-    except:
-        print("Did you add the userbot to the logger group? please check again...")
+  except:
+      print("Did you add the userbot to the logger group? please check again...")
 print("assistant started....")
 bot.start()
