68a5c760eade20848935d06484c971926b29edb4
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 00:19:51 +0530

    Update __init__.py

--- bunny/__init__.py
+++ bunny/__init__.py
@@ -1,1 +1,20 @@
+from pyrogram import Client
+from config import *
+from pyrogram import idle
+from config import 
+import sys
+import os
 
+user = Client(":bunny:",
+                 api_id=API_ID,
+                 api_hash=API_HASH, 
+                 session_string=STRING_SESSION,
+                 plugins = dict(root="bunny/modules")
+               )
+
+bot = Client(":bunny:",
+               api_id=API_ID,
+               api_hash=API_HASH, 
+               session_string=BOT_TOKEN,
+               plugins = dict(root="RaBBiTX/modules")
+            )
