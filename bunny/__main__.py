3f8a25061290deded0302289f83e617de48fa018
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 01:01:12 +0530

    Update __main__.py

--- bunny/__main__.py
+++ bunny/__main__.py
@@ -1,7 +1,7 @@
 from bunny import user, bot
 user.start()
 try:
-  await user.send_message(-1001901276605, "Userbot Started Successfully.... !")
+  user.send_message(-1001901276605, "Userbot Started Successfully.... !")
 except:
     print("Did you add the userbot to the logger group? please check again...")
 
