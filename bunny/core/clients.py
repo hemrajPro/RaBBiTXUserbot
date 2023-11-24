4905feb4776936dc4ce16c163c464cf2e6694b28
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 08:10:43 +0530

    Update clients.py

--- bunny/core/clients.py
+++ bunny/core/clients.py
@@ -1,1 +1,11 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
 
+bot = Client(
+     name="bunny",
+     api_id=API_ID,
+     api_hash=API_HASH,
+     bot_token=BOT_TOKEN
+   )
+
+bunny = Client(name="bunny", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
