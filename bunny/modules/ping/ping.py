3cd2d06ea4090440c75ee6de177859c2c8d8f425
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 10:07:44 +0530

    Update ping.py

--- bunny/modules/ping/ping.py
+++ bunny/modules/ping/ping.py
@@ -1,5 +1,8 @@
 from pyrogram import Client, filters
-from bunny.core.clients import bunny as Client
+from bunny.core.clients import bunny as Client 
+from pyrogram.types import Message
+from bunny import startTime
+from bunny import get_uptime
 
 def grt(seconds: int) -> str:
     count = 0
@@ -23,7 +26,27 @@ def grt(seconds: int) -> str:
     time_list.reverse()
     ping_time += ":".join(time_list)
     return ping_time
-    
-@Client.on_message(filters.command("ping", prefixes=".") & filters.me)
+
+TEXT = """
+•─────────────•
+            __𝗣𝗢𝗡𝗚 __          
+•─────────────•
+__**๏ ᴘɪɴɢ »**__ `{}`
+__**๏ ᴜᴘᴛɪᴍᴇ »**__ `{}`
+**__๏ ᴏᴡɴᴇʀ »__** {}
+"""
+
+@Client.on_message(filters.command("ping", hl))
+async def alive_or_ping(_, m):
+    l = await _.get_me()
+    st = time.time()
+    end = time.time()
+    men = l.mention
+    upt = get_uptime(time.time())
+    pong = str((end-st)*1000)[0:5]
+    gtr = grt(int(time.time()-startTime))
+    return await message.edit(TEXT.format(pong, upt, men))
+        
+@Client.on_message(filters.command("iping", prefixes=".") & filters.me)
 async def ping(client, message):
     await message.edit("Pong!")
