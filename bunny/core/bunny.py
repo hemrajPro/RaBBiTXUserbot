57625f793e1764d8d10a3b57a89b6b63878ae230
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 08:25:06 +0530

    Update bunny.py

--- bunny/core/bunny.py
+++ bunny/core/bunny.py
@@ -1,1 +1,19 @@
+from bunny.core.clients import bunny, bot
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from bunny.modules import ALL_MODULES
 
+async def start_user():
+    await bot.start()
+    print("[•bunny•]: єνєяутнιиg ιѕ σк, ѕтαятιиg... уσυя υѕєявσт ρℓєαѕє ωαιт... ⚡")
+    for all_module in ALL_MODULES:
+        importlib.import_module("bunny.modules" + all_module)
+        print(f"[•bunny•] ѕυ¢¢єѕѕfυℓℓу ιмρσятє∂ {all_module} ⚡")
+    await bunny.start()
+    x = await bunny.get_me()
+    print(f"υѕєявσт ѕυ¢¢єѕѕfυℓℓყ ѕтαятє∂ αѕ {x.first_name} ⚡ ")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_user())
