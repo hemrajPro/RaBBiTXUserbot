7a4f1d624d0eeae8922b920bac9a9b8cfb70f763
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 06:51:02 +0530

    Update __init__.py

--- bunny/__init__.py
+++ bunny/__init__.py
@@ -4,16 +4,16 @@ from pyrogram import idle
 import sys
 import os
 
-user = Client(":bunny:",
+user: Client = Client(":bunny:",
                       api_id=API_ID,
                       api_hash=API_HASH, 
                       session_string=STRING_SESSION,
                       plugins = dict(root="Rabbit")
                       )
 
-bot = Client(":bunny:",
+bot: Client = Client(":bunny:",
                      api_id=API_ID,
                      api_hash=API_HASH, 
                      bot_token=BOT_TOKEN,
-                     plugins = dict(root="Rabbit")
+                     plugins = dict(root="Bot")
                      )
