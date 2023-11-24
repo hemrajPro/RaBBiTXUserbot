1961c72ea7fc532403ff328e3a113301231da576
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 08:58:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 from os import getenv
 
+# API_IDS
+API_ID = int(getenv("API_ID", None)) # API_ID get it from my.telegram.org
+API_HASH = getenv("API_HASH", None) # API_HASH get ut from my.telegram.org
 
-# necessary api_ids 
-API_ID = int(getenv("API_ID", None))
-API_HASH = getenv("API_HASH", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-BOT_TOKEN = getenv("BOT_TOKEN",None)
+# SESSIONS
+STRING_SESSION = getenv("STRING_SESSION", None) # SESSION get it by run cmd "python3 RaBBiTgram.py" in heroku consol or locally 
+BOT_TOKEN = getenv("BOT_TOKEN",None) # BOT_TOKEN get it from @BotFather Bot on telegram
 
-# logs
-LOG_ID = getenv("LOG_ID", None)
+# DATABASES
+MONGO_DB_URI = getenv("MONGO_DB_URI", None) # MONGO_DB_URL get it from mongodb.com
 
-# handler
-COMMAND_HANDLER = getenv("COMMAND_HANDLER", None)
+# LOGGGERS
+LOG_GROUP_ID = getenv("LOG_GROUP_ID", None) # LOG_GROUP_ID get it by creating private group on telegram and fill here that's group id
 
-# pics
-ALIVE_PIC = getenv("ALIVE_PIC", None)
-PING_PIC = getenv("PING_PIC", None)
-HELP_PIC = getenv("HELP_PIC", None)
+# HANDLER
+HANDLER = getenv("HANDLER", None) # HANDLER choose your bot command handler
+
+# IMAGES
+ALIVE_PIC = getenv("ALIVE_PIC", None) # ALIVE_PIC a pic link for alive command in bot
+HELP_PIC = getenv("HELP_PIC", None) # HELP_PIC a pic link for help commad in bot
