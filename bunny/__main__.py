312960dd171b526a8daa0ab75f7c1da6f4af593d
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 07:07:43 +0530

    Update __main__.py

--- bunny/__main__.py
+++ bunny/__main__.py
@@ -1,9 +1,13 @@
 from bunny import user, bot
-user.start()
+
+async def start_bot():
+  await user.start()
+  for all_module in ALL_MODULES:
+        importlib.import_module("bunny.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
 try:
   user.send_message(-1001901276605, "Userbot Started Successfully.... !")
 except:
     print("Did you add the userbot to the logger group? please check again...")
+  await bot.start()
 
-print("assistant started....")
-bot.start()
