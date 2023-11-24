e8dbe65c2577dc80fccb989544fe4b5a8410a795
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 09:18:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 from os import getenv
 
-# API_IDS
+# API_IDS ~ my.telegram.org
 API_ID = int(getenv("API_ID", None)) # API_ID get it from my.telegram.org
 API_HASH = getenv("API_HASH", None) # API_HASH get ut from my.telegram.org
 
-# SESSIONS
+# SESSIONS ~ python3 RaBBiTgram.py
 STRING_SESSION = getenv("STRING_SESSION", None) # SESSION get it by run cmd "python3 RaBBiTgram.py" in heroku consol or locally 
 BOT_TOKEN = getenv("BOT_TOKEN",None) # BOT_TOKEN get it from @BotFather Bot on telegram
 
-# DATABASES
+# DATABASES ~ mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None) # MONGO_DB_URL get it from mongodb.com
 
-# LOGGGERS
+# LOGGGERS ~ telegram private group
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", None) # LOG_GROUP_ID get it by creating private group on telegram and fill here that's group id
 
-# HANDLER
+# HANDLER ~ use ( .,-,!,+,-) any symbol like this...
 HANDLER = getenv("HANDLER", None) # HANDLER choose your bot command handler
 
-# IMAGES
+# IMAGES ~ telegram link of pic
 ALIVE_PIC = getenv("ALIVE_PIC", None) # ALIVE_PIC a pic link for alive command in bot
 HELP_PIC = getenv("HELP_PIC", None) # HELP_PIC a pic link for help commad in bot
