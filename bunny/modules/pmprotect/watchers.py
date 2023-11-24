787544cbeec668ee9a947e3dc144ac8637cbf3c5
Geek <74235427+Geektyper@users.noreply.github.com>
2023-11-24 15:32:54 +0530

    Update pmprotect

---
+++ bunny/modules/pmprotect/watchers.py
@@ -0,0 +1,10 @@
+filters_group = 1
+raid_watcher = 2
+echo_watcher = 3
+global_watcher = 4
+pm_watcher = 5
+afk_watcher = 6
+blacklist_watcher = 7
+dnd_watcher = 8
+chatbot_watcher = 9
+protecc_watcher = 10
