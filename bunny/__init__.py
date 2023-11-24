db78a2e046ff2360bd901dacd43e66671b3a6d67
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 08:27:36 +0530

    Update __init__.py

--- bunny/__init__.py
+++ bunny/__init__.py
@@ -1,15 +1,16 @@
-from pyrogram import Client
-from config import *
-from pyrogram import idle
-import sys
-import os
+from bunny.core.clients import bunny, bot
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from bunny.modules import ALL_MODULES
 
-
-bot = Client(
-     name="bunny",
-     api_id=API_ID,
-     api_hash=API_HASH,
-     bot_token=BOT_TOKEN
-   )
-
-bunny = Client(name="bunny", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+async def start_user():
+    await bot.start()
+    print("[•bunny•]: єνєяутнιиg ιѕ σк, ѕтαятιиg... уσυя υѕєявσт ρℓєαѕє ωαιт... ⚡")
+    for all_module in ALL_MODULES:
+        importlib.import_module("bunny.modules" + all_module)
+        print(f"[•bunny•] ѕυ¢¢єѕѕfυℓℓу ιмρσятє∂ {all_module} ⚡")
+    await bunny.start()
+    x = await bunny.get_me()
+    print(f"υѕєявσт ѕυ¢¢єѕѕfυℓℓყ ѕтαятє∂ αѕ {x.first_name} ⚡ ")
+    await idle()
