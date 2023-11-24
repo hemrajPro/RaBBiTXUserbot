40852855e244057b21c34bc700cac8f4406020c2
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 09:27:11 +0530

    Delete bunny/modules/ping.py

--- bunny/modules/ping.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from bunny.core.clients import bunny as Client
-
-@Client.on_message(filters.command("ping", prefixes=".") & filters.me)
-async def ping(client, message):
-    await message.edit("Pong!")
