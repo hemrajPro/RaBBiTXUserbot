fb2f8b3ac2bfdda521be0b8679c6e200f6502275
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 08:36:18 +0530

    Update __main__.py

--- bunny/__main__.py
+++ bunny/__main__.py
@@ -1,6 +1,9 @@
 from bunny import start_user
 from config import *
 import sys
+import asyncio
+import importlib
+from pyrogram import Client, idle
 
 x = False
 
@@ -9,7 +12,7 @@ if not API_ID:
   x = True
 
 if not API_HASH:
-    print("API_HASH not found please check again... ⚡")
+  print("API_HASH not found please check again... ⚡")
     x = True
 
 if not BOT_TOKEN:
@@ -20,7 +23,10 @@ if not STRING_SESSION:
   print("STRING_SESSION not found please check again.... ⚡")
   x = True
   
-  
+if not MONGO_DB_URI:
+  print("MONGO_DB_URI nit found please check again... ⚡")
+  x = True
+
 if ex:
   sys.exit()
   
@@ -28,3 +34,6 @@ try:
   start_user()
 except Exception as e:
   print(e)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_user())
