72cd742e63040ca1edbb51202713930129a78d97
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-24 07:17:03 +0530

    Update __main__.py

--- bunny/__main__.py
+++ bunny/__main__.py
@@ -1,4 +1,8 @@
 from bunny import user, bot
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from bunny.modules import ALL_MODULES
 
 async def start_bot():
   await user.start()
@@ -10,4 +14,10 @@ try:
 except:
     print("Did you add the userbot to the logger group? please check again...")
   await bot.start()
+except Exception as e:
+            print(f"{e}")
+    await idle()
 
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
