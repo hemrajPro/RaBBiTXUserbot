7682efe081609e5641068695c488254fa9327eec
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-25 13:44:39 +0530

    Create clone.py

---
+++ bunny/modules/clone/clone.py
@@ -0,0 +1,70 @@
+from config import HANDLER as hl
+from pyrogram import Client, filters
+from bunny.Database.clonedb import *
+import os
+from bunny.core.clients import bunny as Client
+
+@Client.on_message(filters.command("clone", hl))
+async def cloner(_, m):
+    if not await verify(_, m):
+        return
+    try:
+        id, args = await get_id_and_args(_, m)
+    except:
+        return await eor(m, "Invalid User.")
+    if not await details_exist():
+        return await eor(m, f"Save your current details first by using ' `{hl}save` ', when you use revert, current details will be applied.")
+    ok = await eor(m, "cloning...")
+    user = await _.get_chat(id)
+    dps = []
+    async for y in _.get_chat_photos(id):
+        dps.append(y.file_id)
+    dp = dps[0]
+    if user.bio:
+        bio = user.bio
+    else:
+        bio = ""
+    x = await _.send_photo("me", dp)
+    dp = await x.download()
+    await x.delete()
+    try:
+        await _.set_profile_photo(photo=dp)
+    except:
+        await _.set_profile_photo(video=dp)
+    await _.update_profile(first_name=user.first_name, last_name=user.last_name if user.last_name else "", bio=bio)
+    await ok.edit("Cloned successfully ✅")
+    os.remove(dp)
+    
+@Client.on_message(filters.command("save", hl))
+async def save(_, m):
+    if not await verify(_, m):
+        return
+    me = await _.get_chat((await my_info()).id)
+    try:
+        dps = []
+        async for y in _.get_chat_photos(me.id):
+            dps.append(y.file_id)
+        dp_id = dps[0]
+    except:
+        dp_id = None
+    details = {"first_name": me.first_name, "last_name": me.last_name if me.last_name else "", "bio": me.bio if me.bio else "", "file_id": dp_id}
+    await save_details(details)
+    await eor(m, "details saved successfully ✅")
+
+@Client.on_message(filters.command("revert", hl))
+async def revert(_, m):
+    if not await verify(_, m):
+        return
+    ok = await eor(m, "Reverting back...")
+    details = await get_details()
+    if details["file_id"]:
+        x = await _.send_photo("me", details["file_id"])
+        dp = await x.download()
+        try:
+            await _.set_profile_photo(photo=dp)
+        except:
+            await _.set_profile_photo(video=dp)
+        await x.delete()
+    await _.update_profile(first_name=details["first_name"], last_name=details["last_name"] if details["last_name"] else "", bio=details["bio"] if details["bio"] else "")
+    await ok.edit("Reverted back ✅")
+    os.remove(dp)
